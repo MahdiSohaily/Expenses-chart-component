6ef7a9a1ac4c66630e2223795f48fa2aa2128ff5
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-08-25 13:47:10 +0430

    Add javascript code to add tag innerHTML

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const items = document.getElementsByClassName("g-item");
+const tags = document.getElementsByClassName("tag");
 fetch("./data.json")
   .then((response) => response.json())
   .then((json) => createGraph(json));
@@ -10,11 +11,13 @@ function createGraph(json) {
   
   for (let counter = 0; counter < json.length; counter++) {
     let elem = items[counter];
+    let tag = tags[counter];
     if (max < json[counter].amount) {
       max = json[counter].amount;
       index = counter;
     }
     elem.style.height = json[counter].amount+10 + "%";
+    tag.innerHTML = "$"+json[counter].amount;
   }
   maxElem =  items[index];
   maxElem.style.backgroundColor = 'hsl(186, 34%, 60%)';
