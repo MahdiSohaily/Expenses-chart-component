cb0dec23ce97bbd90c80b7e8d43e4704c56bb404
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-08-25 13:16:14 +0430

    Add javascript code to read data from external json file

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
-// const items = document.getElementsByClassName("g-item");
-// fetch("./data.json")
-//   .then((response) => response.json())
-//   .then((json) => createGraph(json));
-
-// function createGraph(json) {
-//   let max= 0;
-//   let index= 0;
-//   let maxElem = null;
-  
-//   for (let counter = 0; counter < json.length; counter++) {
-//     let elem = items[counter];
-//     if (max < json[counter].amount) {
-//       max = json[counter].amount;
-//       index = counter;
-//     }
-//     elem.style.height = json[counter].amount+10 + "%";
-//   }
-//   maxElem =  items[index];
-//   maxElem.style.backgroundColor = 'hsl(186, 34%, 60%)';
-// }
+const items = document.getElementsByClassName("g-item");
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((json) => createGraph(json));
